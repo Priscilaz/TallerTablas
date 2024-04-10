aa50cc22cd3547fafd46e3f7f6fa80188896564d
PriscilaZ <priscila.zuniga@udla.edu.ec>
2024-04-10 11:19:17 -0500

    Comentarios a clase Burger.cs

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -1,6 +1,15 @@
-﻿namespace PriscilaZuniga_WebCodeFirst.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriscilaZuniga_WebCodeFirst.Models
 {
     public class Burger
     {
+        public int BurgerID { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        [Range(0.01, 9999.99)]
+        public decimal Precio { get; set; }
+        
     }
 }
