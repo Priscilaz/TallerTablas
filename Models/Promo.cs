a270d5885e8e924186c6c92e1767bfb16bfb4688
Prisz <priscila.zuniga@udla.edu.ec>
2024-04-17 11:28:45 -0500

    Creacion clase Promo

---
+++ Models/Promo.cs
@@ -0,0 +1,10 @@
+﻿namespace PriscilaZuniga_WebCodeFirst.Models
+{
+    public class Promo
+    {
+        public int PromoID { get; set; }
+        public string? Description { get; set; }
+        public DateTime FechaPromo { get; set; }
+
+    }
+}
